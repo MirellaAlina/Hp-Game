f6cbfcfb84e46d97d385245f69bc3dde9f73ef45
Mirella <mirella.alina@gmail.com>
2020-11-14 16:20:50 -0300

    Add wand body

--- src/ar/com/ada/online/second/hpgame/wand/Wand.java
+++ src/ar/com/ada/online/second/hpgame/wand/Wand.java
@@ -1,4 +1,37 @@
 package ar.com.ada.online.second.hpgame.wand;
 
 public class Wand {
+
+    public String name;
+    public Integer damageBoost;
+
+    public Wand(){
+
+    }
+
+    public Wand(String name, Integer damageBoost){
+        this.name = name;
+        this.damageBoost = damageBoost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDamageBoost() {
+        return damageBoost;
+    }
+
+    public void setDamageBoost(Integer damageBoost) {
+        this.damageBoost = damageBoost;
+    }
+
+    @Override
+    public String toString() {
+        return name + "+ dmageBoost" + damageBoost ;
+    }
 }
