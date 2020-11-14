57475ca29010e13848a31b547f58595c6fbdb4c4
Mirella <mirella.alina@gmail.com>
2020-11-14 16:15:39 -0300

    Add wizard body

--- src/ar/com/ada/online/second/hpgame/character/Wizard.java
+++ src/ar/com/ada/online/second/hpgame/character/Wizard.java
@@ -1,4 +1,72 @@
 package ar.com.ada.online.second.hpgame.character;
 
-public class Wizard {
+public class Wizard extends Character {
+
+    //ATRIBUTOS
+    private String wand;
+
+    public Wizard() {
+    }
+
+
+    public Wizard(String name, Integer energyLevel, Integer magicLevel, String location, String wand) {
+        super(name, energyLevel, magicLevel, location);
+        this.wand = wand;
+    }
+
+
+    public String getWand() {
+        return wand;
+    }
+
+    public void setWand(String wand) {
+        this.wand = wand;
+    }
+
+
+    public Boolean darkWizardOrNot() {
+        return null;
+    }
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void Attack() {
+
+
+    }
+
+    @Override
+    public void Healing() {
+
+    }
+
+    @Override
+    public void magicRecovery() {
+
+    }
+
+    @Override
+    public void aliveOrNot() {
+
+    }
+
+    @Override
+    public void addSpell() {
+
+    }
+
+    @Override
+    public void characterStatus() {
+
+    }
 }
