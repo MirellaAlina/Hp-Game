ccc9a94804e5e4bbdec7f06817ec00dca415b957
Mirella <mirella.alina@gmail.com>
2020-11-18 12:40:49 -0300

    update main

--- src/ar/com/ada/online/second/hpgame/Main.java
+++ src/ar/com/ada/online/second/hpgame/Main.java
@@ -3,6 +3,7 @@ package ar.com.ada.online.second.hpgame;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Juego de combate de Harry Potter");
+
     }
 }
