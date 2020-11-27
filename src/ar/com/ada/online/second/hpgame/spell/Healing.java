13a0797b1c0783cd8f94334de3c532ff9e1dc559
Iara <iara_joana@hotmail.com>
2020-11-27 20:06:10 -0300

    Override added

--- src/ar/com/ada/online/second/hpgame/spell/Healing.java
+++ src/ar/com/ada/online/second/hpgame/spell/Healing.java
@@ -18,4 +18,23 @@ public class Healing extends Spell{
     public void setHealingEnergy(int healingEnergy) {
         this.healingEnergy = healingEnergy;
     }
+
+    @Override
+    public Integer getMagicEnergy() {
+        return super.getMagicEnergy();
+    }
+
+    @Override
+    public void setMagicEnergy(Integer magicEnergy) {
+        super.setMagicEnergy(magicEnergy);
+    }
+
+    @Override
+    public String toString() {
+        return   "Healing Spell:"+
+                "\n Name: " + name +
+                "\n Energy restored: " + healingEnergy +
+                "\n Magic energy consumed" + magicEnergy + "\n";
+        //"Magic energy available" cómo mostrar la energía que queda?;
+    }
 }
