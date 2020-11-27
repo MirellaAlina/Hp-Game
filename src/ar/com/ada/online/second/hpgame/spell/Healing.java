62a266aebe1dc3d88aa2487488b5a7c3b6378665
Iara <iara_joana@hotmail.com>
2020-11-27 20:02:15 -0300

    getters and setters added

--- src/ar/com/ada/online/second/hpgame/spell/Healing.java
+++ src/ar/com/ada/online/second/hpgame/spell/Healing.java
@@ -1,4 +1,21 @@
 package ar.com.ada.online.second.hpgame.spell;
 
-public class Healing {
+public class Healing extends Spell{
+    public int healingEnergy;
+
+    public Healing() {
+    }
+
+    public Healing(String name, int magicEnergy, int healingEnergy) {
+        super(name, magicEnergy);
+        this.healingEnergy = healingEnergy;
+    }
+
+    public int getHealingEnergy() {
+        return healingEnergy;
+    }
+
+    public void setHealingEnergy(int healingEnergy) {
+        this.healingEnergy = healingEnergy;
+    }
 }
